d78a0e614aa65b6980b4e619b33d175b3eb6b03b
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-03-23 19:03:46 -0300

    :rotating_light: Fix: Fixing Flake8 error of complexity

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    if not target_time:
-        return None
-
     counter = 0
-    for time in permanence_period:
-        try:
-            if time[0] <= target_time <= time[1]:
+    try:
+        for start, end in permanence_period:
+            if start <= target_time <= end:
                 counter += 1
-        except TypeError:
-            return None
+    except TypeError:
+        return None
     return counter
